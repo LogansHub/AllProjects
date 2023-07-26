07eb0fd449300b45b90876a519f25d0893393c47
loganpeoplesclick <loganashok666@gmail.com>
2023-07-26 06:27:46 +0530

    cs

---
+++ src/condition.java
@@ -0,0 +1,26 @@
+
+public class condition 
+
+{
+	
+	 
+	       public static void main(String args[])
+	    {    
+	       
+	       int age = 15;
+	       
+	       if(age>=18)
+	       {
+	    	   System.out.println("No  he is not eligible");
+	       }
+	       else if(age>=19) 
+	       {
+	    	   System.out.println("No he is not eligible");
+	       }
+	       else if(age>14) 
+	       {
+	    	   System.out.println("he is not eligible");
+	       }
+
+	       System.out.println("Program  executed");
+}}
