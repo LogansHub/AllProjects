741e82fe01db2b02e33243feea5ad446b0b65d1e
loganpeoplesclick <loganashok666@gmail.com>
2023-07-28 06:54:07 +0530

    ei

---
+++ src/edu/org/test/mod/ExceptionClasses.java
@@ -0,0 +1,21 @@
+package edu.org.test.mod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExceptionClasses 
+{
+
+    public static void main(String[] args)
+    {
+		
+        
+		 String s = null;
+		 
+    	
+    	System.out.println(s.length());
+    	
+	}
+    
+	
+}
