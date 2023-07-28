e2a7eb3e6c1c15589f13e55e9867e8196bc2cb26
loganpeoplesclick <loganashok666@gmail.com>
2023-07-28 07:05:16 +0530

    ei

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,44 @@
+package org.add;
+
+public class GreensTech
+{
+	
+	private void greensOmr()
+	{
+	
+		System.out.println("yes greensOmr");
+
+	}
+	private void greensAdayar()
+	{
+	System.out.println("yesgreensAdayar");
+
+	}
+	private void greensTamabaram()
+	{
+
+		System.out.println("yes greensTambaram");
+	}
+	private void greensVelacherry()
+	{
+
+		System.out.println("yes greensvelacherry");
+	}
+	private void greensAnnanagar()
+	{
+	System.out.println("yes greensAnnanager");
+
+	}
+	public static void main(String[] args)
+	{
+		
+		GreensTech g = new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTamabaram();
+		g.greensVelacherry();
+		g.greensAnnanagar();
+	}
+		
+
+}
