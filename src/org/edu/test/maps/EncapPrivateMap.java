23774b3ae2d7fb21f5a6e03dcb306b349c242827
loganpeoplesclick <loganashok666@gmail.com>
2023-07-28 08:30:46 +0530

    ei

---
+++ src/org/edu/test/maps/EncapPrivateMap.java
@@ -0,0 +1,31 @@
+package org.edu.test.maps;
+
+public class EncapPrivateMap 
+{
+
+	 public static void main(String[] args)
+	 
+	 {
+		
+		 
+		 
+		 for(int i= 0;i<=4;i++)
+		 {
+		 
+			 for(int j=0;j==0;j++)
+			 {
+				 
+			   System.out.println("*");
+			 }
+			 
+				
+			 if(i==4)
+			 {
+				 
+				 System.out.println("*********");
+			 }
+			 
+		 }		 
+		 
+	}
+}
