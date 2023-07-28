ce4163bc511cae2f9e278948710f9e6b7cfb0e33
loganpeoplesclick <loganashok666@gmail.com>
2023-07-28 08:22:08 +0530

    ei

---
+++ src/edu/green/test/mod2/Star.java
@@ -0,0 +1,35 @@
+package edu.green.test.mod2;
+
+public class Star
+{
+     
+	 public static void main(String[] args)
+	 {
+		
+		 int i,j;
+		 for(i= 1;i<=5;i++)
+		{
+			
+			 
+		     for(j=i;j<=5;j++)
+		     { 	 
+			 
+		    	 System.out.print(" ");
+		    	 
+		     }
+		     
+		     for(j=1;j<=i;j++)
+		     { 	 
+			 
+		    	 System.out.print("*");
+		    	 
+		     }
+		     
+	     System.out.println();
+		}   } } 
+		 
+		 
+	 
+	
+	
+
