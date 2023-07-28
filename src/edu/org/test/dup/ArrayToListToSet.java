33c29d239d6b4876132d91d3ee41c63e4a4ca631
loganpeoplesclick <loganashok666@gmail.com>
2023-07-28 08:20:12 +0530

    ei

---
+++ src/edu/org/test/dup/ArrayToListToSet.java
@@ -0,0 +1,50 @@
+package edu.org.test.dup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ArrayToListToSet 
+
+{
+    public static void main(String[] args) {
+		
+	
+	
+	  int     a[] =  new    int [5];
+	 
+	   a[0]= 1 ;
+	   a[1] = 2;
+	   a[2] = 3;
+	   a[3] = 4;
+	   a[4] = 5;
+	   for (int i = 0; i <5; i++)
+	   {
+		
+            System.out.println(a[i]);
+	   }
+	   
+	  // List<Integer> l = new ArrayList<>(Arrays.asList(a));
+	  
+	   
+	   
+	   
+	   
+	   
+	  
+	   
+	 
+    
+    
+	
+     
+	
+	
+	
+    }	 
+	
+}
---
+++ src/edu/org/test/dup/DupList.java
@@ -0,0 +1,34 @@
+package edu.org.test.dup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DupList
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		List l = new ArrayList();
+		
+		l.add(50);
+		l.add(100);
+		l.add(50);
+		l.add(200);
+		List n= new ArrayList();
+		for (Object obj : l) ;
+		{
+			
+			
+		/*	if( !n.contains(obj))
+			{
+			
+				n.add(obj);
+		*/	}
+		}
+		
+	}
+	
+	
+
