8fd54afdb427f5165bba45c76ab58b9d3a124a06
loganpeoplesclick <loganashok666@gmail.com>
2023-07-28 08:37:14 +0530

    ei

---
+++ src/operator.java
@@ -0,0 +1,15 @@
+
+public class operator
+{
+	public static void main(String args[])
+	{
+
+        System.out.println("Hi Logan");
+        
+        int a = 10;
+  
+        a--;
+        
+        System.out.println(a);
+	}
+}
